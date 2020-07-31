4b43d02210dea896363fde62a84e7f5eb984a516
Robert Liotta <bob@bobdot.me>
2020-07-31 15:35:49 +0000

    Updates

--- .github/scripts/readme.sh
+++ .github/scripts/readme.sh
@@ -1,11 +1,15 @@
 #!/bin/bash 
 
 
-CV=$(cat VERSION)
+CV=$(cat LATEST_VERSION)
 echo Version: $CV
 
 if echo "$OSTYPE" | grep -q 'darwin' ; then sedopts='-i .bak'; else sedopts='-i'; fi
 
 
 sed ${sedopts} -e "1 s/v.*$/$CV)/g" README.md
+sed ${sedopts} -e "s/release\/v.*/$CV)/g" README.md
+
+
+
 
--- .github/scripts/version.sh
+++ .github/scripts/version.sh
@@ -2,12 +2,12 @@
 
 
 CV=$(cat VERSION)
-echo Version: $CV
+echo "Version: $CV"
 
 if echo "$OSTYPE" | grep -q 'darwin' ; then sedopts='-i .bak'; else sedopts='-i'; fi
 
 sed ${sedopts} -e "s/^\# Version\:.*$/# Version: $CV/g" k8s_shortcuts
 
 echo result:
-echo "kubectl-bob-version: $(grep 'Version:' .k8s_shortcuts)"
+echo "kubectl-bob-version: $(grep 'Version:' k8s_shortcuts)"
 
