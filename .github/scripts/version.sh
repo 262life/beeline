d939e30086110f20b413e9741d4b1905c1c00f3f
Robert Liotta <bob@bobdot.me>
2020-07-31 12:27:34 +0000

    Initial Release

--- .github/scripts/version.sh
+++ .github/scripts/version.sh
@@ -6,7 +6,7 @@ echo Version: $CV
 
 if echo "$OSTYPE" | grep -q 'darwin' ; then sedopts='-i .bak'; else sedopts='-i'; fi
 
-sed ${sedopts} -e "s/^\# Version\:.*$/# Version: $CV/g" .k8s_shortcuts
+sed ${sedopts} -e "s/^\# Version\:.*$/# Version: $CV/g" k8s_shortcuts
 
 echo result:
 echo "kubectl-bob-version: $(grep 'Version:' .k8s_shortcuts)"
