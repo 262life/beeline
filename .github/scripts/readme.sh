642f14e577125e39d351d58dad39068c7d6e3134
Robert Liotta <bob@bobdot.me>
2020-07-31 20:38:31 +0000

    Fixes

--- .github/scripts/readme.sh
+++ .github/scripts/readme.sh
@@ -6,8 +6,9 @@ echo Version: "$CV"
 
 if echo "$OSTYPE" | grep -q 'darwin' ; then sedopts='-i .bak'; else sedopts='-i'; fi
 
-sed ${sedopts} -e "s/release\/v.*\?/release\/$CV\?/g" README.md
-sed ${sedopts} -e "s/build\%20v.*\)/build\%20$CV\)/g" README.md
+sed ${sedopts} -e "s/release\/v.*?/release\/$CV\?/g" README.md
+sed ${sedopts} -e "s/build\%20v.*/build%20$CV)/g" README.md
+
 
 
 
