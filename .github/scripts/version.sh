2de766def9bc929296a94883b347eda9050a695b
Robert Liotta <bob.liotta@rsliotta.com>
2022-04-17 13:23:50 -0400

    Small fix

--- .github/scripts/version.sh
+++ .github/scripts/version.sh
@@ -1,13 +1,12 @@
 #!/bin/bash 
 
-
 CV=$(cat VERSION)
 echo Version: "$CV"
 
 if echo "$OSTYPE" | grep -q 'darwin' ; then sedopts='-i .bak'; else sedopts='-i'; fi
 
 sed ${sedopts} -e "s/^\# Version\:.*$/# Version: $CV/g" beeline.sh
-sed ${sedopts} -e "s/^\beeline - Version\:.*$/beeline - Version: $CV/g" beeline.sh
+sed ${sedopts} -e "s/beeline - Version\:.*$/beeline - Version: $CV/g" beeline.sh
 
 echo result:
 echo "kubectl-bob-version: $(grep 'Version:' beeline.sh)"
