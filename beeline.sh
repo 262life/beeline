7c550c6ecf4a1d930624ada52ed516f34731a4f9
Robert Liotta <44572134+rsliotta@users.noreply.github.com>
2022-05-05 13:13:34 -0400

    Improvements - Update logic

--- beeline.sh
+++ beeline.sh
@@ -213,7 +213,7 @@ update_beeline() {
   curl -s -L https://github.com/262life/beeline/releases/${release}/beeline.sh > "${HOME}/.beeline.k8sX"  && \
   mv "${HOME}/.beeline.k8sX" "${HOME}/.beeline.k8s"  && \
   update_last_updated_file && \
-  exec $0 || \
+  source "${HOME}/.beeline.k8s"  || \
   echo "${cyan}Updating failed...${defcolor} "
 
 }
